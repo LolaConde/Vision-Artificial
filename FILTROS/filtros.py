9e58bbd87ea4396ca9cae0ed824637013065310c
LolaConde <lolacondeherrera@gmail.com>
2024-02-28 13:34:03 +0100

    refactor(FILTROS): Crear clase "Filtro" y subclases
    Para que el código sea ampliable, se ha creado la clase filtro y subclases, de modo que para añadir un filtro sólo se debe de crear una nueva subclase.

--- FILTROS/filtros.py
+++ FILTROS/filtros.py
@@ -7,13 +7,16 @@ from scipy.ndimage import minimum_filter, maximum_filter # Filtros de mínimo y 
 from umucv.util import Help # Para mostrar la ayuda
 import numpy as np
 
-cv.namedWindow("filtros.py") # Crea una ventana llamada "filtros.py"
-cv.moveWindow("filtros.py", 0, 0) # Mueve la ventana a la posición (0, 0), es decir, a la esquina superior izquierda
-region = ROI("filtros.py") # Crea una región de interés en la ventana "filtros.py"
-filtroActual = "0" # Filtro actual (0, 1, ...)
-color = True # Si es true, la imagen es en color, si es false, en blanco y negro
-showHelp = True # Si es true, muestra la ayuda, si es false, la oculta
-onlyRoi = False # Si es true, solo muestra el roi, si es false, lo muestra todo
+# Crea la ventana por primera vez
+nombreVentana = "filtros.py" 
+cv.namedWindow(nombreVentana)
+cv.moveWindow(nombreVentana, 0, 0)
+# Crea la región de interés
+region = ROI(nombreVentana)
+
+color = True # ¿Imagen a color?
+showHelp = True # ¿Mostrar la ayuda?
+onlyRoi = False # ¿Mostrar solo la región de interés?
 
 # Ayuda
 help = Help(
@@ -39,148 +42,220 @@ h: show/hide help
 """)
 Help.show(help) # Inicia enseñando la ayuda
 
+class Filtro:
+    def __init__(self, nombre, id): # Constructor
+        self.nombre = nombre
+        self.id = id
+    def applyFilter(self, frame): # Aplica el filtro a un frame
+        return frame
+    def crearTrackbar(self, nombreVentana): # Crea los trackbars necesarios para el filtro
+        return
+
+class NoFiltro(Filtro):
+    def __init__(self):
+        super().__init__("Sin filtro", "0")
+
+class Gaussian(Filtro):
+    def __init__(self):
+        self.desvEst = 3
+        super().__init__("Gaussian", "2")
+    
+    def applyFilter(self, frame):
+        auto = (0,0)
+        return cv.GaussianBlur(frame, auto, self.desvEst)
+    
+    def crearTrackbar(self, nombreVentana):
+        def guardarValorTrackbar(valor):
+            if valor == 0: valor = 1
+            self.desvEst = valor
+        cv.createTrackbar("DesvEst",  nombreVentana, self.desvEst, 30, guardarValorTrackbar)
+
+class Box(Filtro):
+    def __init__(self):
+        self.tamKernel = 11
+        super().__init__("Box", "1")
+    
+    def applyFilter(self, frame):
+        return cv.boxFilter(frame, -1, (self.tamKernel,self.tamKernel))
+    
+    def crearTrackbar(self, nombreVentana):
+        def guardarValorTrackbar(valor):
+            if valor == 0: valor = 1
+            self.tamKernel = valor
+        cv.createTrackbar("Tam kernel",  nombreVentana, self.tamKernel, 30, guardarValorTrackbar)
+
+class Median(Filtro):
+    def __init__(self):
+        self.tamKernel = 17
+        super().__init__("Median", "3")
+    
+    def applyFilter(self, frame):
+        return cv.medianBlur(frame, self.tamKernel)
+    
+    def crearTrackbar(self, nombreVentana):
+        def guardarValorTrackbar(valor):
+            if valor % 2 == 0: valor += 1
+            self.tamKernel = valor
+        cv.createTrackbar("Tam kernel",  nombreVentana, self.tamKernel, 30, guardarValorTrackbar)
+
+class Bilateral(Filtro):
+    def __init__(self):
+        self.sigmaColor = 15
+        self.sigmaSpace = 15
+        super().__init__("Bilateral", "4")
+    
+    def applyFilter(self, frame):
+        return cv.bilateralFilter(frame, 0, self.sigmaColor, self.sigmaSpace)
+    
+    def crearTrackbar(self, nombreVentana):
+        def guardarValorTrackbar(valor):
+            self.sigmaColor = valor
+        def guardarValorTrackbar2(valor):
+            self.sigmaSpace = valor
+        cv.createTrackbar("Sigma color", nombreVentana, self.sigmaColor, 50, guardarValorTrackbar)
+        cv.createTrackbar("Sigma space", nombreVentana, self.sigmaSpace, 50, guardarValorTrackbar2)
+
+class Min(Filtro):
+    def __init__(self):
+        self.tamKernel = 11
+        super().__init__("Min", "5")
+    
+    def applyFilter(self, frame):
+        return minimum_filter(frame, self.tamKernel)
+    
+    def crearTrackbar(self, nombreVentana):
+        def guardarValorTrackbar(valor):
+            if valor == 0: valor = 1
+            self.tamKernel = valor
+        cv.createTrackbar("Tam kernel",  nombreVentana, self.tamKernel, 30, guardarValorTrackbar)
+
+class Max(Filtro):
+    def __init__(self):
+        self.tamKernel = 11
+        super().__init__("Max", "6")
+    
+    def applyFilter(self, frame):
+        return maximum_filter(frame, self.tamKernel)
+    
+    def crearTrackbar(self, nombreVentana):
+        def guardarValorTrackbar(valor):
+            if valor == 0: valor = 1
+            self.tamKernel = valor
+        cv.createTrackbar("Tam kernel",  nombreVentana, self.tamKernel, 30, guardarValorTrackbar)
+
+class TransformacionDeValor(Filtro):
+    def __init__(self):
+        self.valor = 50
+        super().__init__("Transformación de valor", "7")
+    
+    def applyFilter(self, frame):
+        # Transforma el valor de luminancia de cada píxel independientemente de su entorno, sumándole "valor".
+        yuv = cv.cvtColor(frame, cv.COLOR_BGR2YUV)
+        yuv[:,:,0] = yuv[:,:,0] + self.valor
+        return cv.cvtColor(yuv, cv.COLOR_YUV2BGR)
+    
+    def crearTrackbar(self, nombreVentana):
+        def guardarValorTrackbar(valor):
+            self.valor = valor
+        cv.createTrackbar("Valor",  nombreVentana, self.valor, 100, guardarValorTrackbar)
+
+class EcualizadorDeHistograma(Filtro):
+    def __init__(self):
+        super().__init__("Ecualizador de histograma", "8")
+    
+    def applyFilter(self, frame):
+        # Normalización de contraste mediante ecualizador de histograma.
+        # Se normaliza el contraste de la componente de luminancia (Y).
+        yuv = cv.cvtColor(frame, cv.COLOR_BGR2YUV)
+        yuv[:,:,0] = cv.equalizeHist(yuv[:,:,0])
+        return cv.cvtColor(yuv, cv.COLOR_YUV2BGR)
+
+class CLAHE(Filtro):
+    def __init__(self):
+        super().__init__("CLAHE", "9")
+    
+    def applyFilter(self, frame):
+        # Normaliza el contraste de una imagen. Se aplica CLAHE a la componente de luminancia.
+        clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        yuv = cv.cvtColor(frame, cv.COLOR_BGR2YUV)
+        yuv[:,:,0] = clahe.apply(yuv[:,:,0])
+        return cv.cvtColor(yuv, cv.COLOR_YUV2BGR)
+
+class Opening(Filtro):
+    def __init__(self):
+        super().__init__("Opening", "a")
+    
+    def applyFilter(self, frame):
+        # Este filtro realiza una erosión seguida de una dilatación. Se usa para eliminar ruido. 
+        # Se aplica a la componente de luminancia.
+        yuv = cv.cvtColor(frame, cv.COLOR_BGR2YUV)
+        yuv[:,:,0] = cv.morphologyEx(yuv[:,:,0], cv.MORPH_OPEN, np.ones((5,5),np.uint8))
+        return cv.cvtColor(yuv, cv.COLOR_YUV2BGR)
+
+def setFiltro(idFiltro): # Si no existe, sigue con el filtro que estaba
+    global filtroActual
+    global filtros
+    if filtroActual.id == idFiltro: return
+    for filtro in filtros:
+        if filtro.id == idFiltro:
+            volver_a_crear_Ventana()
+            filtroActual = filtro
+            filtroActual.crearTrackbar(nombreVentana)
+            return
+
+# Inicializa el filtro actual
+filtroActual = NoFiltro()
+# Crea una lista con todos los filtros
+subclasesDeFiltro = Filtro.__subclasses__()
+filtros = []
+for subclase in subclasesDeFiltro:
+    filtros.append(subclase())
+
 def frameToGray(frame):
     # Primero se pasa a gris
     gris = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # Después hace que vuelva a tener 3 canales (BGR)
     return cv.cvtColor(gris, cv.COLOR_GRAY2BGR)
 
-def frameToBox(frame, valor):
-    # Le aplica el filtro box, que suaviza la imagen.
-    # El primer parámetro es la imagen, el segundo es el tipo de salida (si es -1, la salida es la misma que la entrada), y cuanto más grande sea el tercero (el tamaño del kernel), más suavizada estará la imagen
-    return cv.boxFilter(frame, -1, (valor,valor))
+def volver_a_crear_Ventana():
+    cv.destroyWindow(nombreVentana)
+    cv.namedWindow(nombreVentana)
+    global region
+    region = ROI(nombreVentana)
+    region.roi = [x1, y1, x2, y2]
 
-def frameToGaussian(frame, desvEst):
-    auto = (0,0) # tamaño de la máscara automático, dependiendo de desvEst (la desviación estándar de la distribución gaussiana)
-    # A cuanto mayor valor de desviación, más suavizada estará la imagen. 
-    return cv.GaussianBlur(frame, auto, desvEst)
-
-def frameToMedian(frame, valor):
-    # El valor es el tamaño del kernel
-    # A más valor, más suavizada está la imagen. Si subo de 17, da error
-    return cv.medianBlur(frame, valor)
-
-def frameToBilateral(frame, sigmaColor, sigmaSpace):
-    # Promedia los píxeles cercanos que además tienen un valor similar
-    # Cuanto más grande es el sigmaColor, píxeles con valores más alejados se promedian
-    # Cuanto más grande es el sigmaSpace, píxeles más alejados se promedian
-    return cv.bilateralFilter(frame, 0, sigmaColor, sigmaSpace)
-
-def frameToMin(frame, valor):
-    # Filtro de mínimo. El valor modifica la cantidad de vecinos que se tienen en cuenta
-    return minimum_filter(frame, valor)
-
-def frameToMax(frame, valor):
-    # Filtro de máximo. El valor modifica la cantidad de vecinos que se tienen en cuenta
-    return maximum_filter(frame, valor)
-
-def transformacionDeValor(frame, valor):
-    # Transforma el valor de luminancia de cada píxel independientemente de su entorno, sumándole "valor".
-    yuv = cv.cvtColor(frame, cv.COLOR_BGR2YUV)
-    yuv[:,:,0] = yuv[:,:,0] + valor
-    # Se pasa de YUV a BGR para que la imagen vuelva a tener el mismo formato que al principio
-    return cv.cvtColor(yuv, cv.COLOR_YUV2BGR)
-
-def normalizacionDeContraste(frame):
-    # Normalización de contraste mediante ecualizador de histograma.
-    # Se normaliza el contraste de la componente de luminancia (Y).
-    yuv = cv.cvtColor(frame, cv.COLOR_BGR2YUV)
-    yuv[:,:,0] = cv.equalizeHist(yuv[:,:,0])
-    return cv.cvtColor(yuv, cv.COLOR_YUV2BGR)
-
-def clahe(frame):
-    # CLAHE. Normaliza el contraste de una imagen. Se aplica CLAHE a la componente de luminancia.
-    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    yuv = cv.cvtColor(frame, cv.COLOR_BGR2YUV)
-    yuv[:,:,0] = clahe.apply(yuv[:,:,0])
-    return cv.cvtColor(yuv, cv.COLOR_YUV2BGR)
-
-def opening(frame):
-    # Este filtro realiza una erosión seguida de una dilatación. Se usa para eliminar ruido. 
-    # Se aplica a la componente de luminancia.
-    yuv = cv.cvtColor(frame, cv.COLOR_BGR2YUV)
-    yuv[:,:,0] = cv.morphologyEx(yuv[:,:,0], cv.MORPH_OPEN, np.ones((5,5),np.uint8))
-    return cv.cvtColor(yuv, cv.COLOR_YUV2BGR)
-
-trozo = None # Trozo de imagen
 # Recorrer todos los fotogramas del flujo de entrada
 for key, frame in autoStream():
-    trozo = None # Empieza con la region roi vacía
-    H,W,_ = frame.shape # Alto y ancho de la imagen
-    
-    # Es mejor trabajar con imagen espejo
+    trozo = None
+
     imagecam = cv.flip(frame, 1) # Con modo espejo
     
+    # Si ha pulsado una tecla para indicar un filtro, se guarda cual ha sido
+    setFiltro(chr(key))
+    if key == ord('c'):
+        color = not color
+    if key == ord('r'):
+        onlyRoi = not onlyRoi
+    help.show_if(key, ord('h'))
+    
     if region.roi:
-        [x1, y1, x2, y2] = region.roi # Coordenadas de la región de interés
+        # Guarda la región de interés
+        [x1, y1, x2, y2] = region.roi
         trozo = frame[y1:y2+1, x1:x2+1]
         
-        # Si ha pulsado una tecla para indicar un filtro, se guarda cual ha sido
-        if key == ord('0'):
-            filtroActual = "0" # No hace nada
-        if key == ord('1'):
-            filtroActual = "1" # box
-        if key == ord('2'):
-            filtroActual = "2" # Gaussian
-        if key == ord('3'):
-            filtroActual = "3" # Median
-        if key == ord('4'):
-            filtroActual = "4" # Bilateral
-        if key == ord('5'):
-            filtroActual = "5" # Min
-        if key == ord('6'):
-            filtroActual = "6" # Max
-        if key == ord('7'):
-            filtroActual = "7" # Transformación de valor
-        if key == ord('8'):
-            filtroActual = "8" # Equalizador de histograma
-        if key == ord('9'):
-            filtroActual = "9" # CLAHE
-        if key == ord('a'):
-            filtroActual = "a" # opening
-        if key == ord('c'):
-            color = not color # Color/monochrome
-        if key == ord('r'):
-            onlyRoi = not onlyRoi # Muestra solo roi / muestra todo
-        
-        help.show_if(key, ord('h')) # Muestra/oculta la ayuda
-        
         # Si no está en color, lo pasa a gris
-        if not color:
-            trozo = frameToGray(trozo)
-        
-        # Se aplica el filtro correspondiente (0 no hace nada)
-        if filtroActual == "1":
-            trozo = frameToBox(trozo, 11)
-        elif filtroActual == "2":
-            trozo = frameToGaussian(trozo, 3)
-        elif filtroActual == "3":
-            trozo = frameToMedian(trozo, 17)
-        elif filtroActual == "4":
-            trozo = frameToBilateral(trozo, 20, 20)
-        elif filtroActual == "5":
-            trozo = frameToMin(trozo, 11)
-        elif filtroActual == "6":
-            trozo = frameToMax(trozo, 11)
-        elif filtroActual == "7":
-            trozo = transformacionDeValor(trozo, 100)
-        elif filtroActual == "8":
-            trozo = normalizacionDeContraste(trozo)
-        elif filtroActual == "9":
-            trozo = clahe(trozo)
-        elif filtroActual == "a":
-            trozo = opening(trozo)
-        elif filtroActual == "0":
-            pass
+        if not color: trozo = frameToGray(trozo)
+
+        # Se aplica el filtro correspondiente y se muestra el nombre del filtro
+        trozo = filtroActual.applyFilter(trozo)
+        putText(trozo, filtroActual.nombre, (15, 20))
         frame[y1:y2+1, x1:x2+1] = trozo
+        
         # Se muestra un rectángulo rodeando la región de interés
         cv.rectangle(frame, (x1,y1), (x2,y2), color=(10,10,10), thickness=2)
         
-    # Muestra el fotograma, y si se ha pulsado la recla r y había seleccionada una región de interés, muestra solo esa región. Si se había pulsado pero no había región de interés, muestra todo (deberá marcar ROI de nuevo y después pulsar r)
     if (onlyRoi):
-        if (trozo is not None):
-            cv.imshow("filtros.py", trozo)
-        else:
-            onlyRoi = False
+        if (trozo is not None): cv.imshow(nombreVentana, trozo)
+        else: onlyRoi = False
     if(not onlyRoi):
-        cv.imshow("filtros.py", frame)
+        cv.imshow(nombreVentana, frame)
