6bd4919d121e7cba1a157de568a368cd0c231567
LolaConde <lolacondeherrera@gmail.com>
2024-05-26 18:28:56 +0200

    feat-docs(RECTIF): Pasa archivos por parámetro
    Debía pasar los nombres de los archivos como parámetro, y meter los dos archivos de coordenadas en uno solo.
    
    Falta por hacer el ejemplo con una imagen creada por mí.

--- RECTIF/RECTIF.py
+++ RECTIF/RECTIF.py
@@ -2,23 +2,33 @@
 
 import cv2 as cv
 import numpy as np
+from umucv.stream import sourceArgs # Para obtener los argumentos
+import argparse, sys # Para obtener los argumentos
 
 NOMBRE_VENTANA = 'Selecciona 2 puntos'
 
-# Calcular distancia entre 2 puntos
 def distancia(p1, p2):
     return np.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
 
-# Leer las coordenadas de un archivo
 def leerCoordenadas(nombre_archivo):
+    # Formato:
+    # x1 , y1 - x2 , y2
+    # x3 , y3 - x4 , y4
+    # ...
+    #
+    # Los puntos de la izquierda se devuelven en una lista y los de la derecha en otra lista (en el mismo orden)
     archivo = open(nombre_archivo, 'r')
     lineas = archivo.readlines()
-    puntos = []
+    puntos1 = []
+    puntos2 = []
     for linea in lineas:
-        x, y = linea.strip().split(",")
-        puntos.append((float(x), float(y)))
+        p1, p2 = linea.split("-")
+        x1, y1 = p1.strip().split(",")
+        x2, y2 = p2.strip().split(",")
+        puntos1.append((float(x1), float(y1)))
+        puntos2.append((float(x2), float(y2)))
     archivo.close()
-    return puntos
+    return puntos1, puntos2
 
 # Calcular la transformación entre coordenadas homogéneas y tradicionales
 def homog(x):
@@ -33,22 +43,26 @@ def inhomog(x):
 def htrans(h,x):
     return inhomog(homog(x) @ h.T)
 
-# Obtenemos la imagen
-nombre = input("Introduce el nombre de la imagen (y su ruta): ")
-img = cv.imread(nombre)
+# Obtenemos los argumentos de entrada
+parser = argparse.ArgumentParser()
+parser.add_argument('--imagen', help='Ruta de la imagen', type=str, default=None)
+parser.add_argument('--coordenadas', help='Ruta del archivo con las coordenadas', type=str, default=None)
+sourceArgs(parser)
+args, rest = parser.parse_known_args(sys.argv)
+
+assert len(rest)==1, 'Parámetros desconocidos: '+str(rest[1:])
+if args.imagen is None or args.coordenadas is None:
+    print("Falta alguno de los parámetros obligatorios")
+    sys.exit(1)
+
+img = cv.imread(args.imagen)
 if img is None:
     print('Error: No se ha podido cargar la imagen.')
     exit()
 
-# Obtenemos las coordenadas reales
-nombre_archivo = input("Introduce el nombre del archivo (y su ruta) con las coordenadas reales de los puntos: ")
-puntos_reales = leerCoordenadas(nombre_archivo)
+puntos_imagen, puntos_reales = leerCoordenadas(args.coordenadas)
 
-# Obtenemos las coordenadas en la imagen
-nombre_archivo = input("Introduce el nombre del archivo (y su ruta) con las coordenadas en la imagen: ")
-puntos_imagen = leerCoordenadas(nombre_archivo)
-
-# Obtenemos la homografía
+# Calculamos la homografía
 H, _ = cv.findHomography(np.array(puntos_imagen), np.array(puntos_reales))
 
 # Pedimos que seleccione 2 puntos
@@ -62,9 +76,9 @@ while True:
         global puntos, img, NOMBRE_VENTANA
         if event == cv.EVENT_LBUTTONDOWN:
             if len(puntos) == 0:
-                img = cv.imread(nombre)
+                img = cv.imread(args.imagen)
             puntos.append((x, y))
-            cv.circle(img, (x, y), 5, (255, 0, 255), -1)
+            cv.circle(img=img, center=(x, y), radius=5, color=(255, 0, 255), thickness=-1)
             cv.imshow(NOMBRE_VENTANA, img)
             
     puntos = []
@@ -76,8 +90,10 @@ while True:
     # Calcula los puntos en la imagen rectificada
     puntos_real = htrans(H, puntos)
 
-    # Calcula la distancia entre los puntos
-    distancia_puntos = distancia(puntos_real[0], puntos_real[1])
-    medio_linea = (int((puntos[0][0] + puntos[1][0]) / 2), int((puntos[0][1] + puntos[1][1]) / 2))
-    cv.putText(img, str(round(distancia_puntos, 2)), medio_linea, cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-    cv.line(img, np.array([int(i) for i in puntos[0]]), np.array([int(i) for i in puntos[1]]), (255, 0, 255), 2)
+    # Dibuja la distancia entre los puntos
+    inicio_linea = np.array([int(i) for i in puntos[0]])
+    final_linea = np.array([int(i) for i in puntos[1]])
+    medio_linea = (int((inicio_linea[0] + final_linea[0]) / 2), int((inicio_linea[1] + final_linea[1]) / 2))
+    distancia_puntos = str(round(distancia(puntos_real[0], puntos_real[1]), 2))
+    cv.putText(img=img, text=distancia_puntos, org=medio_linea, fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+    cv.line(img=img, pt1=inicio_linea, pt2=final_linea, color=(255, 0, 255), thickness=2)
